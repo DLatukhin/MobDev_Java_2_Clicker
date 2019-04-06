82d3a284494c9033bfdaebe34e85783f7d5d60f2
goncharovkonstantin <goncharovkostya.1997@gmail.com>
2019-04-07 01:03:18 +0300

    Задание выполнено

--- app/src/main/java/com/example/mobile_task_1_2_clicker/MainActivity.java
+++ app/src/main/java/com/example/mobile_task_1_2_clicker/MainActivity.java
@@ -4,13 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextClock;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView mainText;
-    Button mainBtn;
+    TextView tvScore;
+    ImageButton mainBtn;
+    Button btnDecrease;
+    Button btnDrop;
 
     private long score = 0;
 
@@ -19,20 +23,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mainText = (TextView) findViewById(R.id.mainText);
-        mainBtn = (Button) findViewById(R.id.button);
-
+        mainText =  findViewById(R.id.mainText);
+        tvScore = findViewById(R.id.tvScore);
+        mainBtn = findViewById(R.id.button);
+        btnDecrease = findViewById(R.id.btnDecrement);
+        btnDrop = findViewById(R.id.btnDrop);
 
         View.OnClickListener clickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 score ++;
-                String s = "Кликов: " + score;
-                mainText.setText(s.toCharArray(),0, s.length());
+                tvScore.setText(String.valueOf(score));
             }
         };
 
         mainBtn.setOnClickListener(clickListener);
+
+        btnDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                score = score == 0 ? 0 : score-1;
+                tvScore.setText(String.valueOf(score));
+            }
+        });
+
+        btnDrop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                score = 0;
+                tvScore.setText(String.valueOf(score));
+            }
+        });
 
     }
 }
